1ebdabba035042d1a3a1ee99475ff805c899ebef
Alfaro Sierra <manuel.alfaro-sierra-external@capgemini.com>
2016-06-13 16:55:56 +0200

    ...developing

--- testing/WEB/index.js
+++ testing/WEB/index.js
@@ -14,4 +14,28 @@ $(document).ready(function() {
         $('#t1').fadeOut(2000);
         $('#cc1').fadeIn(3000);
     });
+
+    $('.slider-item ').click(function(ev) {
+        var self = this;
+        $(this).parent().find('.slider-item').each(function(index, value) {
+            reset(this);
+        }).promise().done(function() {
+            toggle(self);
+        });
+    });
 });
+
+function reset(el) {
+    $(el).find('.slider-item-response').css('color', 'black');
+    $(el).find('.slider-item-response').css('background-color', 'white');
+}
+
+function toggle(el) {
+    if ($(el).find('.slider-item-response').css('color') === 'rgb(0, 0, 0)') {
+        $(el).find('.slider-item-response').css('color', 'white');
+        $(el).find('.slider-item-response').css('background-color', 'black');
+    } else {
+        $(el).find('.slider-item-response').css('color', 'black');
+        $(el).find('.slider-item-response').css('background-color', 'white');
+    }
+}
