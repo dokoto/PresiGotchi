ceb457661c729ce406944ed6290e8832313d473e
Alfaro Sierra <manuel.alfaro-sierra-external@capgemini.com>
2016-11-30 16:23:50 +0100

    Developing...

--- presiGotchi/src/js/app/modules/stage/logic/engine.js
+++ presiGotchi/src/js/app/modules/stage/logic/engine.js
@@ -10,7 +10,8 @@ class Engine {
         let str = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
         let spans = '<span>' + str.split('').join('</span><span>') + '</span>';
         let porc = 4;
-        let progress = ($(spans).length * (100 / porc)) / 100;
+        let progressUnit = ($(spans).length * (100 / porc)) / 100;
+        let progress = progressUnit;
         let hiddenHeight = $('#typing-text')[0].scrollHeight - $('#typing-text')[0].clientHeight;
 
         $(spans).hide().appendTo('#typing-text').each(function(i) {
@@ -24,11 +25,16 @@ class Engine {
                     hiddenHeight = $('#typing-text')[0].scrollHeight - $('#typing-text')[0].clientHeight;
                 }
                 if (i >= progress) {
+                    let scroll = hiddenHeight / porc;
+                    if (porc === 2) {
+                        scroll = hiddenHeight;
+                    }
                     $('#typing-text').animate({
-                        scrollTop: hiddenHeight / porc
+                        scrollTop: scroll
                     });
                     porc--;
-                    progress = $(spans).length * (100 / porc) / 100;
+                    //progress = $(spans).length * (100 / porc) / 100;
+                    progress += progressUnit;
                 }
             });
         });
